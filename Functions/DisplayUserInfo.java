4d690ae9326eb4d95f4701d89bc0aa9afdc7765b
ROCO <dorumi714@gmail.com>
2020-11-03 20:25:46 +0900

    revert test

--- Functions/DisplayUserInfo.java
+++ Functions/DisplayUserInfo.java
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 package Functions;
 
 import javax.swing.JOptionPane;
@@ -57,6 +58,36 @@ public class DisplayUserInfo {
 		birthTextArea.setText((String) model.getValueAt(SelectedRow,9));
 =======
 >>>>>>> parent of 6b4cf4d... Functions Complete
+	}
+}
+>>>>>>> parent of 264e4e7... Merge branch 'KYS' of https://github.com/SilicaDurumi/Java_Project into KYS
+=======
+package Functions;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+public class DisplayUserInfo {
+	
+	public static void DisplayUser(JTable table, JTextField numberTextField, JTextField idNumberTextField,
+			JTextArea ageTextArea, JTextArea genderTextArea, JTextArea locationTextArea ,JTextArea birthTextArea, int SelectedRow) {
+		SelectedRow = table.getSelectedRow();
+		if (SelectedRow == -1) {
+			JOptionPane.showMessageDialog(null, "Please Select The Data", "Error", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+		
+		DefaultTableModel model = (DefaultTableModel) table.getModel();
+		
+		numberTextField.setEnabled(false);
+		idNumberTextField.setEnabled(false);
+		ageTextArea.setText((String) model.getValueAt(SelectedRow, 6)); 
+		genderTextArea.setText((String) model.getValueAt(SelectedRow,7));
+		locationTextArea.setText((String) model.getValueAt(SelectedRow,8));
+		birthTextArea.setText((String) model.getValueAt(SelectedRow,9));
 	}
 }
 >>>>>>> parent of 264e4e7... Merge branch 'KYS' of https://github.com/SilicaDurumi/Java_Project into KYS
--- main.java
+++ main.java
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -69,6 +70,74 @@ public class main extends JFrame{
 
 
 
+=======
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+public class main extends JFrame{
+	
+	public main() {
+		
+		OUTTER : while(true) {
+			ImageIcon icon = new ImageIcon("images/Food.JPG");
+			
+			JOptionPane.showMessageDialog(null, null,"Customer Management System", JOptionPane.NO_OPTION, icon);
+			
+			String passwd = JOptionPane.showInputDialog("CM Service\n Write PassWord");
+			
+			String password = "manage1234";
+			
+			if (passwd == null)
+				break OUTTER;
+			 else if (password.equals(passwd)) {
+					try {
+						ManagementSystem frame = new ManagementSystem();
+						frame.setVisible(true);
+						} catch (Exception e) {
+							e.printStackTrace();
+					}
+				 break OUTTER;
+			}else
+				JOptionPane.showMessageDialog(null, "Wrong PassWord\n Click the Confirm Button", "Error", JOptionPane.ERROR_MESSAGE);
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		main authMainExample = new main();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+>>>>>>> parent of 264e4e7... Merge branch 'KYS' of https://github.com/SilicaDurumi/Java_Project into KYS
 =======
 
 import javax.swing.ImageIcon;
