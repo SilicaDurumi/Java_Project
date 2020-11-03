34c912fad1ad44013ada1eaef303075c818edabe
ROCO <dorumi714@gmail.com>
2020-11-03 21:50:05 +0900

    open_Function

--- Functions/OpenFileFunction.java
+++ Functions/OpenFileFunction.java
@@ -3,6 +3,7 @@ package Functions;
 import java.awt.FileDialog;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.StringTokenizer;
 import java.util.Vector;
 
 import javax.swing.JFrame;
@@ -17,41 +18,34 @@ public class OpenFileFunction {
 	    
 	    String path = openFile.getDirectory()+ openFile.getFile();
 	    try{
+	    	String str ="";
+	    	Vector<String> userdataInput = new Vector<String>();
 	        BufferedReader buffread = new BufferedReader(new FileReader(path));
-	        Vector<String> userinfo = new Vector<String>();
+	        Vector<Vector<String>> userdata = new Vector<Vector<String>>();
 	        DefaultTableModel model = (DefaultTableModel) table.getModel();	      
+	        StringTokenizer stringTokenizer;
 	        
-	        for (int i = 0; i < table.getModel().getRowCount(); i++) {
-		    	   for (int j = 0; j < table.getModel().getColumnCount(); j++)
+	        for (int i = 0; i < model.getRowCount(); i++) {
+		    	   for (int j = 0; j < model.getColumnCount(); j++)
 		    		   table.getModel().setValueAt(null, i, j);
 		    model.removeRow(i);
 	        }
 	      
+	        while ((str=buffread.readLine())!= null) {
+	        	stringTokenizer = new StringTokenizer(str);
+				for (int i = 0; i < model.getColumnCount(); i++) 
+					userdataInput.add(stringTokenizer.nextToken(" ### "));
+			userdata.add(userdataInput);
+			}
 	        
-//	      	userinfo.add(buffread.readLine().split(" ### "));
-	       	model.addRow(userinfo);
+	        
+	       	model.addRow(userdata);
 	        buffread.close();
 	        String Filename = openFile.getFile();
 	        frame.setTitle(Filename);
 	       } catch(Exception e1){System.out.println(e1.toString());}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
