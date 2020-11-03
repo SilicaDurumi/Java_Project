344bb3a3570785ab575e05d824b2eb903081dffd
ROCO <dorumi714@gmail.com>
2020-11-03 19:32:06 +0900

    Conflict Test

--- Functions/CreateFunction.java
+++ Functions/CreateFunction.java
@@ -1,61 +1,61 @@
 package Functions;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
-public class CreateFunction{
+public class CreateFunction implements ActionListener{
+	JTable table;
+	JTextField[] userdata = new JTextField[10];
 	
-	public static void CreateFunction(JTable table, JTextField number, JTextField name, JTextField tel , JTextField email, JTextField idNumber, JTextField Job) {
-		JTable tablesub;
-		JTextField[] userdata = new JTextField[10];
-		tablesub = table;
+	public CreateFunction(JTable table, JTextField number, JTextField name, JTextField tel , JTextField email, JTextField idNumber, JTextField Job) {
+		this.table = table;
 		userdata[0] = number;
 		userdata[1] = name;
 		userdata[2] = tel;
 		userdata[3] = email;
 		userdata[4] = idNumber;
 		userdata[5] = Job;
-
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
 		boolean FLAG = false;
 		String[] userinfo = new String[10] ;
 		
 		for (int i = 0; i < userinfo.length-4; i++) 
-			userinfo[i] = userdata[i].getText();
+			userinfo[i] = this.userdata[i].getText();
 		
 		for (int i = 0; i < userinfo.length-4; i++) {
 			if (userinfo[i].length()==0) 			
 				FLAG = true;
 		}
 		if (FLAG) {
-			JOptionPane.showMessageDialog(null, "Please Write all information of the INPUT Field", "Error", JOptionPane.ERROR_MESSAGE );
+			JOptionPane.showMessageDialog(null, "Please Write all information of the TextField", "Error", JOptionPane.ERROR_MESSAGE );
 			return ;
 		}
-		if (ID_NumberCheckExcute.UserIdNumberCheck(userdata[4].getText())) {
-			try {
-				userinfo[6] = ID_NumberCheckExcute.CheckNumberReturnAge(userinfo[4]);
-				userinfo[7] = ID_NumberCheckExcute.CheckNumberReturnGender(userinfo[4]);
-				userinfo[8] = ID_NumberCheckExcute.CheckNumberReturnLocation(userinfo[4]);
-				userinfo[9] = ID_NumberCheckExcute.CheckNumberReturnBirth(userinfo[4]);
-				DefaultTableModel model = (DefaultTableModel) table.getModel();
-				model.addRow(userinfo);
-			} catch (Exception e) {
-				JOptionPane.showMessageDialog(null, "Please Write Valid Number", "Error", JOptionPane.ERROR_MESSAGE);
-			}
-			
+		if (ID_NumberCheckExcute.UserIdNumberCheck(this.userdata[4].getText())) {
+			userinfo[6] = ID_NumberCheckExcute.CheckNumberReturnAge(userinfo[4]);
+			userinfo[7] = ID_NumberCheckExcute.CheckNumberReturnGender(userinfo[4]);
+			userinfo[8] = ID_NumberCheckExcute.CheckNumberReturnLocation(userinfo[4]);
+			userinfo[9] = ID_NumberCheckExcute.CheckNumberReturnBirth(userinfo[4]);
+			DefaultTableModel model = (DefaultTableModel) table.getModel();
+			model.addRow(userinfo);
 		
 			for (int j = 0; j < userinfo.length-4; j++) 
 				userdata[j].setText("");
 				userdata[0].requestFocus();
+				
 		}
 		else {
-			JOptionPane.showMessageDialog(null, "Invalid IDNumber Value Please Write Again","Invalid IDNumer", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null, "Invalid IDNumber Please Write Again","Invalid IDNumer", JOptionPane.ERROR_MESSAGE);
 			return;
 		}
 	}
-
 }
 
 
--- Functions/OpenFileFunction.java
+++ Functions/OpenFileFunction.java
@@ -3,8 +3,8 @@ package Functions;
 import java.awt.FileDialog;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.StringTokenizer;
 import java.util.Vector;
+
 import javax.swing.JFrame;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
@@ -12,43 +12,28 @@ import javax.swing.table.DefaultTableModel;
 public class OpenFileFunction {
 	public static void OpenFile(JFrame frame, JTable table) {//use vector
 		FileDialog openFile;
-		Vector<Vector<String>> userinfo = new Vector<Vector<String>>();
-		DefaultTableModel model = (DefaultTableModel) table.getModel();
-		StringTokenizer tokenizer;
-		String str ="";
-		int count = 0;
-		
 		openFile = new FileDialog(frame, "OPEN", FileDialog.LOAD);
 	    openFile.setVisible(true); 
 	    
 	    String path = openFile.getDirectory()+ openFile.getFile();
-	    
 	    try{
-	    	 BufferedReader buffread = new BufferedReader(new FileReader(path));
-	    	
-	    	for (int i = 0; i < table.getModel().getRowCount(); i++) {
+	        BufferedReader buffread = new BufferedReader(new FileReader(path));
+	        Vector<String> userinfo = new Vector<String>();
+	        DefaultTableModel model = (DefaultTableModel) table.getModel();	      
+	        
+	        for (int i = 0; i < table.getModel().getRowCount(); i++) {
 		    	   for (int j = 0; j < table.getModel().getColumnCount(); j++)
 		    		   table.getModel().setValueAt(null, i, j);
-		    	   model.removeRow(i);
+		    model.removeRow(i);
 	        }
-	    	
-	    	while ((str=buffread.readLine())!=null) {
-	    		
-	    		tokenizer = new StringTokenizer(str);
-	    	for (int i = 0; i <count; i++) { 
-	    		for (int j = 0; j < table.getColumnCount(); j++) {
-//					System.out.println(tokenizer.nextToken(" ### ")); 
-	    			userinfo.get(i).add(j, tokenizer.nextToken(" ### "));
-	    		}
-	    		if (tokenizer.hasMoreTokens()) {count++;	}
-	    }
-}
-	    	model.addRow(userinfo);
-	    	buffread.close();
+	      
+	        
+//	      	userinfo.add(buffread.readLine().split(" ### "));
+	       	model.addRow(userinfo);
+	        buffread.close();
 	        String Filename = openFile.getFile();
 	        frame.setTitle(Filename);
-		} catch (Exception e1) {
-			/* System.out.println(e1.toString()); */}
+	       } catch(Exception e1){System.out.println(e1.toString());}
 	}
 }
 
