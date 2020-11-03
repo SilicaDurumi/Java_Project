6b4cf4dc268fdb7c19614e6ba9c56e09727631ff
IT <i5-2500@S.H>
2020-11-03 17:57:25 +0900

    Functions Complete
    Save, Delete, ADD, Table Select Enable, Disable, Edit Function

--- Functions/EditFunction.java
+++ Functions/EditFunction.java
@@ -1,17 +1,21 @@
 package Functions;
 
 
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 public class EditFunction {
 	public static void UpdateData(JTable table, String name , String tel, String email, int selectedrow) {
+		if (selectedrow==-1) {
+			JOptionPane.showMessageDialog(null, "Please Select the Data","Error",JOptionPane.ERROR_MESSAGE);
+			return;
+		}
 		
 		DefaultTableModel model = (DefaultTableModel) table.getModel();
 		
 		model.setValueAt(name, selectedrow,1);
 		model.setValueAt(tel, selectedrow, 2);
 		model.setValueAt(email, selectedrow, 3);
-		
 	}
 }
